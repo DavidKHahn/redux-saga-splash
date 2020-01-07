b1f9bc53a49cb2b23c2de5131893e965e1742a82
David <dhahn123@gmail.com>
2020-01-06 18:02:06 -0800

    sagaMiddleware.run(rootSaga);

--- src/store/index.js
+++ src/store/index.js
@@ -1,7 +1,7 @@
 import { applyMiddleware, compose, createStore } from 'redux';
 import createSagaMiddleware from 'redux-saga';
 import rootReducer from '../reducers';
-
+import rootSaga from '../sagas;';
 
 const configureStore = () => {
     const sagaMiddleware = createSagaMiddleware()
@@ -13,6 +13,7 @@ const configureStore = () => {
             window.__REDUX_DEVTOOLS_EXTENSION__()
         )
     );
+    sagaMiddleware.run(rootSaga);
     return store;
 }
 
