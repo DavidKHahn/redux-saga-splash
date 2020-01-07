35aefa44beffd899c27d633d67faab269ffb55a9
David <dhahn123@gmail.com>
2020-01-07 14:59:55 -0800

    handleImagesLoad saga

--- src/sagas/index.js
+++ src/sagas/index.js
@@ -1,23 +1,14 @@
-import { call, put, take } from 'redux-saga/effects';
+import { takeEvery } from 'redux-saga/effects';
+import { IMAGES } from '../constants';
+
 // whenever a saga comes across a promise it will then pause until promise is completed
-function* workerSaga() {
-    console.log('Hey from worker');
-    console.log(put({ type: 'ACTION_FROM_WORKER' }))
-    yield put({ type: 'ACTION_FROM_WORKER' });
+function* handleImagesLoad() {
+    console.log('Fetching images from Unsplash');
 }
 
-function* byebyeSaga() {
-    console.log('Bye bye');
-}
 // watcher saga
 function* rootSaga() {
-    // take is useful for one calls such as LOGIN and does not have a callback
-    yield take('LOGIN');
-    yield call(workerSaga);
-    // yield take('ADD_TO_CART');
-    // yield take('BUY');
-    yield take('LOGOUT');
-    yield call(byebyeSaga);
+    takeEvery(IMAGES.LOAD, handleImagesLoad);
 }
 
 // watcher saga listens for an action then invoke worker saga
