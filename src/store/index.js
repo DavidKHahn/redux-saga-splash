7ff4af7c5947cd0369abe25cb46f95bb335da6a4
David <dhahn123@gmail.com>
2020-01-06 17:59:22 -0800

    applyMiddleware, compose in store

--- src/store/index.js
+++ src/store/index.js
@@ -1,12 +1,17 @@
-import { createStore } from 'redux';
+import { applyMiddleware, compose, createStore } from 'redux';
+import createSagaMiddleware from 'redux-saga';
 import rootReducer from '../reducers';
 
 
 const configureStore = () => {
+    const sagaMiddleware = createSagaMiddleware()
     const store = createStore(
         rootReducer,
-        window.__REDUX_DEVTOOLS_EXTENSION__ &&
-        window.__REDUX_DEVTOOLS_EXTENSION__()
+        compose(
+            applyMiddleware(sagaMiddleware),
+            window.__REDUX_DEVTOOLS_EXTENSION__ &&
+            window.__REDUX_DEVTOOLS_EXTENSION__()
+        )
     );
     return store;
 }
