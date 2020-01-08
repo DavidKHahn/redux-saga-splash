77cebc45f7f8e77a41d5385cbd232f484b0e8b79
David <dhahn123@gmail.com>
2020-01-08 15:28:53 -0800

    api: fetchImageStats, yield fork: statsSaga

--- src/sagas/statsSaga.js
+++ src/sagas/statsSaga.js
@@ -1,16 +1,31 @@
-import { fork, take } from 'redux-saga/effects';
+import { call, fork, put, take } from 'redux-saga/effects';
+import { loadImagesStats, setImagesStats, setImagesStatsError } from '../actions';
+import { fetchImageStats } from '../api';
 import { IMAGES } from '../constants';
 
-
 function* handleStatsRequest(id) {
-    console.log('fetching Stats for ', id);
+    // makes requests to load/fetch image stats
+    // runs iteration 3x at the most and fails if does not pass after 3 iterations
+    for (let i = 0; i < 3; i++) {
+        try {
+            yield put(loadImagesStats(id));
+            const res = yield call(fetchImageStats, id);
+            yield put(setImagesStats(id, res.downloads.total));
+            return true;
+        } catch (e) { }
+    }
+
+    yield put(setImagesStatsError(id));
 }
+
 
 export default function* watchStatsRequest() {
     while (true) {
         const { images } = yield take(IMAGES.LOAD_SUCCESS);
 
         for (let i = 0; i < images.length; i++) {
+            // fork will run sagas in parallel
+            // call would have suspended until particular saga has run
             yield fork(handleStatsRequest, images[i].id);
         }
     }
