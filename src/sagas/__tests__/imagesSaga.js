6a800359d450f381e8518336b8419936b61b250a
David <dhahn123@gmail.com>
2020-01-08 16:53:39 -0800

    testing error handling with redux saga

--- src/sagas/__tests__/imagesSaga.js
+++ src/sagas/__tests__/imagesSaga.js
@@ -1,5 +1,5 @@
 import { runSaga } from 'redux-saga';
-import { setImages } from '../../actions';
+import { setError, setImages } from '../../actions';
 import * as api from '../../api';
 import { getPage, handleImagesLoad } from '../imagesSaga';
 test('selector gives back the page', () => {
@@ -27,4 +27,24 @@ test('should load images and handle them in case of success', async () => {
     // assert that this action was dispatched for setImages
     // dispatching mockedImages
     expect(dispatchedActions).toContainEqual(setImages(mockedImages));
+})
+
+test('should handle errors in case of failure', async () => {
+    // dispatched actions
+    const dispatchedActions = [];
+    // mocked function use
+    const error = 'Some error is thrown';
+    api.fetchImages = jest.fn(() => Promise.reject(error));
+
+    const fakeStore = {
+        getState: () => ({ nextPage: 1 }),
+        dispatch: action => dispatchedActions.push(action)
+    }
+    await runSaga(fakeStore, handleImagesLoad).done;
+    // assertions
+    // mock functions should have been invoked only once
+    expect(api.fetchImages.mock.calls.length).toBe(1);
+    // assert that this action was dispatched for setError
+    // dispatch error
+    expect(dispatchedActions).toContainEqual(setError(error));
 })
